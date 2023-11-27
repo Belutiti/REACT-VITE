e456fae9bc48b1fd82b81bcb6967787d660c7f53
belutiti <belen.tiaye@gmail.com>
2023-11-27 10:56:56 -0300

    creacion de url dinamico y componente Detail

--- ctd-esp-fe3-final/src/Routes/Home.jsx
+++ ctd-esp-fe3-final/src/Routes/Home.jsx
@@ -1,34 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import Card from '../Components/Card'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import Card from "../Components/Card";
+import axios from "axios";
+import { Link } from "react-router-dom"
+
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
-  const url='https://jsonplaceholder.typicode.com/users'
-  const [dentistas, setDentistas]= useState([])
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const [dentistas, setDentistas] = useState([]);
 
-const apiCall=()=>{
-  axios.get(url)
-  .then((data)=>(
-  setDentistas(data.data)
-  ))
-}
+  const apiCall = () => {
+    axios.get(url).then((data) => setDentistas(data.data));
+  };
 
-useEffect(()=>{
-apiCall()
-}, [])
+  useEffect(() => {
+    apiCall();
+  }, []);
 
   return (
-    <main className="" >
+    <main className="">
       <h1>Home</h1>
-      <div className='card-grid'>
-        {dentistas.map((dentista)=>{
-          return <Card key={dentista.id} name={dentista.name} username={dentista.username} id={dentista.id}/>
-        })}
+      <div className="card-grid">
+        {dentistas.map((dentista) => (
+          <Link key={dentista.id} to={`/dentista/${dentista.id}`}>
+            <Card
+              key={dentista.id}
+              name={dentista.name}
+              username={dentista.username}
+              id={dentista.id}
+            />
+          </Link>
+          ))}
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
